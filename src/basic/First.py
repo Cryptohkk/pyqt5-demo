b390a40ae049fcd1846c6f5a121921d1df257c35
hkkhuang <hkkhuang@163.com>
2020-03-24 00:32:56 +0800

    delete pdf

--- src/basic/First.py
+++ src/basic/First.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication, QWidget
 
 if __name__ == '__main__':
     # 创建QApplication类的实例
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     # 创建一个窗口
     w = QWidget()
     # 设置窗口的尺寸
-    w.resize(400,200)
+    w.resize(400, 200)
     # 移动窗口
-    w.move(300,300)
+    w.move(300, 300)
 
     # 设置窗口的标题
     w.setWindowTitle('第一个基于PyQt5的桌面应用')
